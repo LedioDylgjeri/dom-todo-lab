2a1ded50ac552c00fafc63a177d5f856bd745bb0
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-08-30 20:46:35 -0400

    complete DOM lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+//      ------ Cached Elements ------
+const input = document.getElementById('input');
+const btn = document.getElementById('submit-button')
+const uList = document.getElementById('todo-list')
+
+btn.addEventListener('click', getList);
+
+function getList(list) {
+  const listElement = document.createElement('li')
+  // console.log(listElement);
+  listElement.textContent = input.value
+  uList.appendChild(listElement)
+  input.value = ''
+}
+
 
